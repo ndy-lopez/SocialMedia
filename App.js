f4486f6a7c20173afe48b7f50b27820e8e805293
Andy Loredo <ndy.loredo@gmail.com>
2023-11-16 11:23:18 -0500

    Create title component and add props with style

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text
-        style={{
-          fontSize: 50,
-          fontFamily: getFontFamily({baseFont: 'Inter', weight: '100'}),
-        }}>
-        Hello World!
-      </Text>
+      <Title title={"Let's Explore"} />
     </SafeAreaView>
   );
 };

ac0cd41665e9f24e3d637fae7e539a405bc79e80
Andy Loredo <ndy.loredo@gmail.com>
2023-11-20 10:39:33 -0500

    Set up UserProfileImage component and its styles

---
+++ components/UserProfileImage/style.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  userImageContainer: {
+    borderColor: '#F35BAC',
+    borderWidth: 1,
+    padding: 3,
+    borderRadius: 50,
+  },
+  image: {
+    width: 65,
+    height: 65,
+  },
+});
+
+export default style;
